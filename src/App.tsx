6a06e6173948e34e9b2615a7faf773e5b99b8d4e
DannyMacha25 <Danny_Machado@student.uml.edu>
2025-05-30 12:17:35 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap"
+import Welcome from "./Welcome"
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <Container fluid>
+      <Welcome />
+    </Container>
+  )
+}
+
+export default App
---
+++ src/Welcome.tsx
@@ -0,0 +1,51 @@
+import { Container, Image, Row, Col} from "react-bootstrap"
+import banner from "./assets/fridgie_black_banner.png"
+
+import './Welcome.css'
+
+const GoogleForm = () => {
+    return (
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScjLgBftIuaet5t51eT-RC7lAWJFx9d6SuNwaaKRqcLbNKlyw/viewform?embedded=true" 
+        width="100%" height="400">Loading…</iframe>
+    );
+}
+
+function Welcome() {
+    return (
+        <Container className="justify-content-center align-items-center" fluid>
+            {/* Logo Banner*/}
+            <Row className="mt-5 logo">
+                <Col />
+                <Col>
+                    <Image width={"100%"} height={"100%"} fluid src={banner} />
+                </Col>
+                <Col />
+                    
+            </Row>
+            
+            
+            <Row className="mt-3 mb-5 slogan">
+                <Col />
+                <Col className="text-center">
+                    <span>
+                        The future of food tracking and saving{' '}
+                        <span className="text-success">money</span>
+                    </span>
+                </Col>
+                <Col />
+            </Row>
+
+            <Row className="survey">
+                <Col xs={2}/>
+                <Col xs={8}>
+                    <GoogleForm />
+                    <p className="mt-5 text-center text-muted">{"(c) Fridgie LLC. 2025"}</p>
+                </Col>
+                <Col xs={2}/>
+            </Row>
+                    
+        </Container>
+    )
+}
+
+export default Welcome
