94ce02119adac32a74808bd61b36bd70551e84b0
DannyMacha25 <Danny_Machado@student.uml.edu>
2025-05-30 12:41:04 -0400

    Made changes to look nicer on phones

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -15,33 +15,33 @@ function Welcome() {
         <Container className="justify-content-center align-items-center" fluid>
             {/* Logo Banner*/}
             <Row className="mt-5 logo">
-                <Col />
-                <Col>
+                <Col xs={1} lg={4}/>
+                <Col xs={10} lg={4}>
                     <Image width={"100%"} height={"100%"} fluid src={banner} />
                 </Col>
-                <Col />
+                <Col xs ={1} lg={4}/>
                     
             </Row>
             
             
             <Row className="mt-3 mb-5 slogan">
-                <Col />
-                <Col className="text-center">
+                <Col md={1} lg={4}/>
+                <Col md={10} lg={4} className="text-center">
                     <span>
                         The future of food tracking and saving{' '}
                         <span className="text-success">money</span>
                     </span>
                 </Col>
-                <Col />
+                <Col md={1} lg={4}/>
             </Row>
 
             <Row className="survey">
-                <Col xs={2}/>
-                <Col xs={8}>
+                <Col xs={0} lg={2}/>
+                <Col xs={12} lg={8}>
                     <GoogleForm />
                     <p className="mt-5 text-center text-muted">{"(c) Fridgie LLC. 2025"}</p>
                 </Col>
-                <Col xs={2}/>
+                <Col xs={0} lg={2}/>
             </Row>
                     
         </Container>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fridgie-site/',  // repo name here, with leading and trailing slash
   plugins: [react()],
 })
